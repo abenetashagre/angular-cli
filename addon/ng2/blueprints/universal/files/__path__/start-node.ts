4a56d3c111a138ea4f6a781bfa09c529a1ab49eb
Johannes Werner <johannes.werner86@gmail.com>
2016-06-17 20:45:52 +0200

    use serve-static instead of express.static

--- addon/ng2/blueprints/universal/files/__path__/start-node.ts
+++ addon/ng2/blueprints/universal/files/__path__/start-node.ts
@@ -1,9 +1,6 @@
 import * as path from 'path';
 import * as express from 'express';
-
-/**
- * Angular 2 universal
- */
+import * as serveStatic from 'serve-static';
 import 'angular2-universal/polyfills';
 import {
   enableProdMode,
@@ -16,11 +13,7 @@ import {
   BootloaderConfig,
   NODE_PLATFORM_PIPES
 } from 'angular2-universal';
-
-/**
- * ServerSide app
- */
-import { AppComponent, environment } from './app/index';
+import {AppComponent, environment} from './app/index';
 
 let app = express();
 let port = process.env.PORT || 4200;
@@ -28,37 +21,30 @@ let root = path.join(path.resolve(__dirname), '..');
 
 enableProdMode();
 
-/**
- * Render HTML files with the Angular 2 Universal ExpressEngine
- */
 app.engine('html', expressEngine);
 app.set('views', root);
 app.set('view engine', 'html');
 app.set('view options', {doctype: 'html'});
 app.set('json spaces', 2);
 
-/**
- * static content for SPA
- */
-app.use(express.static(path.join(root, 'dist')));
+app.use(serveStatic(path.join(root, 'dist'), {index: false}));
+app.use(serveStatic(path.join(root, 'public'), {index: false}));
 
-/**
- * Render Angular 2 Application
- * @param req
- * @param res
- */
-function ngApp(req, res) {
-  let options: BootloaderConfig = {
+function ngApp(req, res, next) {
+  if (req.path === '/ember-cli-live-reload.js') {
+    next();
+    return;
+  }
+  let options:BootloaderConfig = {
     async: true,
     directives: [AppComponent],
     platformProviders: [
-      { provide: ORIGIN_URL, useValue: 'file://' },
-      { provide: BASE_URL, useValue: '/' }
+      {provide: ORIGIN_URL, useValue: 'file://'},
+      {provide: BASE_URL, useValue: '/'}
     ],
     preboot: false,
     providers: [
-      { provide: REQUEST_URL, useValue: req.originalUrl },
-
+      {provide: REQUEST_URL, useValue: req.originalUrl},
       NODE_PLATFORM_PIPES,
       NODE_ROUTER_PROVIDERS,
       NODE_HTTP_PROVIDERS
@@ -68,14 +54,11 @@ function ngApp(req, res) {
   res.render('dist/index', options);
 }
 
-/**
- * render everything else on the server
- */
-app.get('/**', ngApp);
+app.get('*', ngApp);
 
 if (environment.production) {
   app.listen(port, () => {
-    console.log('Listening on http://localhost:' + port);
+    console.log('Listening on port:' + port);
   });
 }
 
